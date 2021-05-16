74f13a567662855ee1b617688a73b61ea8013dd5
Derek Bruckner <dbrucknr@umich.edu>
2021-05-16 17:58:04 -0400

    first clean up, as project started out exploratory - this commit removes access files, adds better organization.

--- backend/location/api/serializers.py
+++ backend/location/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Zipcode
+from location.models import Zipcode
 
 class ZipcodeSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/location/api/views.py
+++ backend/location/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Zipcode
+from location.models import Zipcode
 from .serializers import ZipcodeSerializer
 # Search and Filter zipcode packages:
 from rest_framework import filters
